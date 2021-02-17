6abc79a5e0c6632334ccb16f9317968379634ee5
tshenry <taylor@discourse.org>
2021-02-17 15:48:13 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Discourse Custom Composer Placeholder
+
+A theme component that allows a site to use custom placeholder text in specified categories and/or for specific user trust levels.
---
+++ javascripts/discourse/api-initializers/init-custom-composer-placeholders.js
@@ -0,0 +1,35 @@
+import { apiInitializer } from "discourse/lib/api";
+import discourseComputed from "discourse-common/utils/decorators";
+
+export default apiInitializer("0.8", (api) => {
+  const rawSplitSettings = settings.custom_placeholder.split("|");
+
+  api.modifyClass("component:composer-editor", {
+    @discourseComputed("composer.requiredCategoryMissing")
+    replyPlaceholder(requiredCategoryMissing) {
+      let textKey;
+
+      rawSplitSettings.forEach((rawSetting) => {
+        const settingParts = rawSetting.split("~");
+        const placeholderText = settingParts[0];
+        const categoryIds = settingParts[1] ? settingParts[1].split(",") : [];
+        const trustLevels = settingParts[2] ? settingParts[2].split(",") : [];
+
+        const showForCategory = categoryIds.some(
+          (c) => parseInt(c) === this.composer._categoryId || c === "all"
+        );
+        const showForTrustLevel = trustLevels.some(
+          (t) => parseInt(t) === this.currentUser.trust_level || t === "all"
+        );
+
+        if (showForCategory && showForTrustLevel) {
+          textKey = themePrefix(
+            `custom_composer_placeholder.text_${placeholderText}`
+          );
+        }
+      });
+
+      return textKey ? textKey : this._super(requiredCategoryMissing);
+    },
+  });
+});
