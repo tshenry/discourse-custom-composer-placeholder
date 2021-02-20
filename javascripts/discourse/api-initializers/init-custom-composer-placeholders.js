bcfc6a76673fbb685e407502a3efc21da306ed1a
tshenry <taylor@discourse.org>
2021-02-19 18:41:48 -0800

    Make sure to limit to topic creation

--- javascripts/discourse/api-initializers/init-custom-composer-placeholders.js
+++ javascripts/discourse/api-initializers/init-custom-composer-placeholders.js
@@ -8,27 +8,27 @@ export default apiInitializer("0.8", (api) => {
     @discourseComputed("composer.requiredCategoryMissing")
     replyPlaceholder(requiredCategoryMissing) {
       let textKey;
+      if (this.composer.action === "createTopic") {
+        rawSplitSettings.forEach((rawSetting) => {
+          const settingParts = rawSetting.split("~");
+          const placeholderText = settingParts[0];
+          const categoryIds = settingParts[1] ? settingParts[1].split(",") : [];
+          const trustLevels = settingParts[2] ? settingParts[2].split(",") : [];
 
-      rawSplitSettings.forEach((rawSetting) => {
-        const settingParts = rawSetting.split("~");
-        const placeholderText = settingParts[0];
-        const categoryIds = settingParts[1] ? settingParts[1].split(",") : [];
-        const trustLevels = settingParts[2] ? settingParts[2].split(",") : [];
-
-        const showForCategory = categoryIds.some(
-          (c) => parseInt(c) === this.composer._categoryId || c === "all"
-        );
-        const showForTrustLevel = trustLevels.some(
-          (t) => parseInt(t) === this.currentUser.trust_level || t === "all"
-        );
-
-        if (showForCategory && showForTrustLevel) {
-          textKey = themePrefix(
-            `custom_composer_placeholder.text_${placeholderText}`
+          const showForCategory = categoryIds.some(
+            (c) => parseInt(c) === this.composer._categoryId || c === "all"
           );
-        }
-      });
+          const showForTrustLevel = trustLevels.some(
+            (t) => parseInt(t) === this.currentUser.trust_level || t === "all"
+          );
 
+          if (showForCategory && showForTrustLevel) {
+            textKey = themePrefix(
+              `custom_composer_placeholder.text_${placeholderText}`
+            );
+          }
+        });
+      }
       return textKey ? textKey : this._super(requiredCategoryMissing);
     },
   });
